021e5cea88197d5d326db38b4335392b40e3f4b9
Yalamber <yalamber@mundhum.com>
2024-08-13 00:40:25 +0545

    submit link

--- src/app/add-link/page.tsx
+++ src/app/add-link/page.tsx
@@ -1,25 +1,30 @@
-
 import connectMongo from '@/utils/dbConnect';
 import FilterOption from '@/models/FilterOption';
-import {
-  Section
-} from '@telegram-apps/telegram-ui';
+import { Section } from '@telegram-apps/telegram-ui';
 
 async function getData() {
   await connectMongo();
-  const categories = await FilterOption.find({ type: 'CATEGORY' }).lean();
-  const countries = await FilterOption.find({ type: 'COUNTRY' }).lean();
-  const languages = await FilterOption.find({ type: 'LANGUAGE' }).lean();
-  return [categories, countries, languages];
+
+  const filterTypes = ['CATEGORY', 'COUNTRY', 'LANGUAGE'];
+
+  const [categories, countries, languages] = await Promise.all(
+    filterTypes.map((type) => FilterOption.find({ type }).lean())
+  );
+
+  return { categories, countries, languages };
 }
 
 import AddForm from './addForm';
 
 export default async function AddLinkPage() {
-  const [categories, countries, languages] = await getData();
+  const { categories, countries, languages } = await getData();
   return (
     <Section header="Channel/Group Details">
-      <AddForm categories={JSON.parse(JSON.stringify(categories))} countries={JSON.parse(JSON.stringify(countries))} languages={JSON.parse(JSON.stringify(languages))} />
+      <AddForm
+        categories={JSON.parse(JSON.stringify(categories))}
+        countries={JSON.parse(JSON.stringify(countries))}
+        languages={JSON.parse(JSON.stringify(languages))}
+      />
     </Section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,21 @@ import Link from '@/models/Link';
 
 async function getData() {
   await connectMongo();
-  const categories = await FilterOption.find({ type: 'CATEGORY' }).lean();
-  const countries = await FilterOption.find({ type: 'COUNTRY' }).lean();
-  const languages = await FilterOption.find({ type: 'LANGUAGE' }).lean();
-  const links = await Link.find().lean();
-  return [categories, countries, languages, links];
+
+  const filterTypes = ['CATEGORY', 'COUNTRY', 'LANGUAGE'];
+
+  const [categories, countries, languages, links] = await Promise.all([
+    ...filterTypes.map((type) => FilterOption.find({ type }).lean()),
+    Link.find().lean(),
+  ]);
+
+  return { categories, countries, languages, links };
 }
 
 export const revalidate = 120;
 
 export default async function Home() {
-  const [categories, countries, languages, links] = await getData();
+  const { categories, countries, languages, links } = await getData();
   return (
     <HomePage
       links={JSON.parse(JSON.stringify(links))}
