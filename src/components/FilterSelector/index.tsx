4f992f22cdb9d595e80db1eaa83d122756f94934
Yalamber <yalamber@mundhum.com>
2024-08-15 19:20:24 +0545

    added infinite scroll

---
+++ src/components/FilterSelector/index.tsx
@@ -0,0 +1,97 @@
+import { ReactElement, useState } from 'react';
+import {
+  Modal,
+  Button,
+  Placeholder,
+  List,
+  Navigation,
+  Divider,
+} from '@telegram-apps/telegram-ui';
+import { Icon28Close } from '@telegram-apps/telegram-ui/dist/icons/28/close';
+
+interface FilterProps {
+  icon: ReactElement;
+  items: Array<{ label: string; value: string }>;
+  label: string;
+  onChange: (value: string | null) => void;
+  disabled?: boolean;
+  triggerClass?: string;
+}
+
+const FilterSelector = ({
+  icon,
+  items,
+  label = 'select',
+  disabled = false,
+  onChange,
+  triggerClass,
+}: FilterProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Modal
+      className="z-20"
+      open={isOpen}
+      header={
+        <Modal.Header
+          after={
+            <button
+              onClick={() => {
+                onChange(null);
+                setIsOpen(false);
+              }}
+            >
+              <Icon28Close style={{ color: 'var(--tgui--plain_foreground)' }} />
+            </button>
+          }
+        >
+          <Placeholder description="Description" header="Title">
+            {label}
+          </Placeholder>
+        </Modal.Header>
+      }
+      onOpenChange={(open) => {
+        setIsOpen(open);
+      }}
+      trigger={
+        <Button
+          before={<>{icon}</>}
+          disabled={disabled}
+          size="s"
+          mode="plain"
+          onClick={() => setIsOpen(items?.length > 0 ? true : false)}
+          className={`${triggerClass ?? ''}`}
+        >
+          {label}
+        </Button>
+      }
+    >
+      <div className="m-2 px-5">
+        <List
+          className="cursor-pointer"
+          onClick={() => {
+            onChange(null);
+            setIsOpen(false);
+          }}
+        >
+          <Navigation className="pt-2">Not selected</Navigation>
+          <Divider />
+        </List>
+        {items.map((item) => (
+          <List
+            key={item.value}
+            className="cursor-pointer"
+            onClick={() => {
+              onChange(item.value);
+              setIsOpen(false);
+            }}
+          >
+            <Navigation className="pt-2">{item.label}</Navigation>
+            <Divider />
+          </List>
+        ))}
+      </div>
+    </Modal>
+  );
+};
+
+export default FilterSelector;
---
+++ src/components/InfiniteLinkList/index.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { useEffect, useState, useRef } from 'react';
+import LinkDisplay from '../LinkDisplay';
+import { Spinner } from '@telegram-apps/telegram-ui';
+
+export default function InfiniteLinkList({
+  initialLinks,
+}: {
+  initialLinks: Array<any>;
+}) {
+  const [links, setLinks] = useState(initialLinks);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const loadMore = async () => {
+    if (hasMoreData) {
+      //TODO: api call
+      const apiLinks: Array<any> = [];
+
+      if (apiLinks.length == 0) {
+        setHasMoreData(false);
+      }
+      setLinks((prevLinks) => [...prevLinks, ...apiLinks]);
+    }
+
+    console.log('Load more called');
+  };
+  const scrollTrigger = useRef(null);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !window.IntersectionObserver) {
+      return;
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadMore();
+        }
+      },
+      { threshold: 0.5 }
+    );
+    if (scrollTrigger.current) {
+      observer.observe(scrollTrigger.current);
+    }
+    // Cleanup
+    return () => {
+      if (scrollTrigger.current) {
+        observer.unobserve(scrollTrigger.current);
+      }
+    };
+  }, [hasMoreData]);
+
+  return (
+    <div>
+      {links.map((item: any) => (
+        <LinkDisplay item={item} />
+      ))}
+      <div className="p-4">
+        {hasMoreData && (
+          <div ref={scrollTrigger}>
+            <Spinner size="m" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
