190c4b79039ab34a0e130c652daedaee2cdd5a3e
Yalamber <yalamber@mundhum.com>
2024-08-29 10:07:40 +0545

    fix importer

--- bot-server/ecosystem.config.ts
+++ bot-server/ecosystem.config.ts
@@ -6,12 +6,6 @@ module.exports = {
       interpreter: 'bun',
       interpreter_args: '--env-file=.env',
       watch: true,
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
