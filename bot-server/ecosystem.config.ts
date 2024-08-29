34ab7b80c5fcb06f6b567b82906f062eb2a7a8d6
Yalamber <yalamber@mundhum.com>
2024-08-29 09:53:18 +0545

    fix importer

--- bot-server/ecosystem.config.ts
+++ bot-server/ecosystem.config.ts
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'TeleHubListBot',
       script: './bot-server/index.ts',
-      interpreter: 'node',
-      interpreter_args: '--experimental-strip-types --env-file=.env',
+      interpreter: 'bun',
+      interpreter_args: '--env-file=.env',
       watch: true,
       env: {
         NODE_ENV: 'development',
