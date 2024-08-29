03f06071c3850483d9a07193922191a492c67c79
Yalamber <yalamber@mundhum.com>
2024-08-29 10:12:07 +0545

    fix importer

--- bot-server/index.ts
+++ bot-server/index.ts
@@ -1,20 +1,22 @@
-import { Bot } from "grammy";
+import { Bot } from 'grammy';
 
 // Create an instance of the `Bot` class and pass your bot token to it.
 const bot = new Bot(process.env.TG_BOT_TOKEN);
 
-const message = 'Open our mini app to browse interesting channels';
-const websiteUrl = 'https://t.me/TeleHubListBot?startapp=TeleHubList';
+console.log('Starting bot...');
 
 // Handle the /start command.
-bot.command('start', (ctx) =>
+bot.command('start', (ctx) => {
+  console.log('start commant received', ctx.from);
+  const message = 'Open our mini app to browse interesting channels';
+  const websiteUrl = 'https://t.me/TeleHubListBot?startapp=TeleHubList';
   ctx.reply(message, {
     parse_mode: 'Markdown',
     reply_markup: {
       inline_keyboard: [[{ text: 'Browse Channels', url: websiteUrl }]],
     },
-  })
-);
+  });
+});
 
 // Handle other messages.
 bot.on('message', (ctx) => ctx.reply('Yo visit our mini app!'));
